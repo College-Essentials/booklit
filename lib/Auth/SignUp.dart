7405a74ee171fda63e08d8fb785e21dfdca45d21
srivats <srivats.venkataraman@gmail.com>
2020-01-03 16:29:35 -0800

    Enhancments

--- lib/Auth/SignUp.dart
+++ lib/Auth/SignUp.dart
@@ -2,7 +2,6 @@ import 'package:booklit/Auth/Login.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:booklit/Navigation.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUp extends StatefulWidget {
