d7a4b8489ef8aa2a1a4df2152949df9dac9a9b2e
srivats <srivats.venkataraman@gmail.com>
2020-01-04 16:47:10 -0800

    Added a logo, and completed accounts page

--- lib/Screens/Account.dart
+++ lib/Screens/Account.dart
@@ -1,8 +1,10 @@
+import 'package:booklit/Auth/Login.dart';
 import 'package:booklit/Auth/sign_in.dart';
 import 'package:booklit/Screens/AccountRelated/UserProfile.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Account extends StatefulWidget {
   @override
@@ -37,65 +39,158 @@ class _AccountState extends State<Account> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Card(
-                      color: Color.fromRGBO(255, 236, 179, 1),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          ListTile(
-                            leading: Icon(
-                              Icons.account_circle,
-                              size: 50,
-                              color: Colors.black,
-                            ),
-                            title: FutureBuilder(
-                              future: FirebaseAuth.instance.currentUser(),
-                              builder: (context,
-                                  AsyncSnapshot<FirebaseUser> snapshot) {
-                                if (snapshot.hasData) {
-                                  String email = snapshot.data.email;
-                                  String userName = email.substring(
-                                      0, email.indexOf("@calstatela.edu"));
-                                  return Text(
-                                    'User Name: ' + userName,
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 15),
-                                  );
-                                } else {
-                                  return Text('Loading...');
-                                }
-                              },
-                            ),
-                            subtitle: FutureBuilder(
-                              future: FirebaseAuth.instance.currentUser(),
-                              builder: (context,
-                                  AsyncSnapshot<FirebaseUser> snapshot) {
-                                if (snapshot.hasData) {
-                                  String email = snapshot.data.email;
-                                  return Text(
-                                    'Email: ' + email,
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 15),
-                                  );
-                                } else {
-                                  return Text('Loading...');
-                                }
-                              },
-                            ),
-                            trailing: GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (BuildContext context) =>
-                                            UserProfile()));
-                              },
-                              child: Icon(Icons.edit),
-                            ),
-                          )
-                        ],
-                      ))
+                  ListTile(
+                    leading: Card(
+                      color: Color.fromRGBO(126, 187, 191, 1),
+                      child: Icon(
+                        Icons.account_circle,
+                        size: 50,
+                        color: Colors.black,
+                      ),
+                    ),
+                    title: FutureBuilder(
+                      future: FirebaseAuth.instance.currentUser(),
+                      builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+                        if (snapshot.hasData) {
+                          String email = snapshot.data.email;
+                          String userName = email.substring(
+                              0, email.indexOf("@calstatela.edu"));
+                          return Text(
+                            'User Name: ' + userName,
+                            style: TextStyle(color: Colors.black, fontSize: 15),
+                          );
+                        } else {
+                          return Text('Loading...');
+                        }
+                      },
+                    ),
+                    subtitle: FutureBuilder(
+                      future: FirebaseAuth.instance.currentUser(),
+                      builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+                        if (snapshot.hasData) {
+                          String email = snapshot.data.email;
+                          return Text(
+                            'Email: ' + email,
+                            style: TextStyle(color: Colors.black, fontSize: 15),
+                          );
+                        } else {
+                          return Text('Loading...');
+                        }
+                      },
+                    ),
+                    trailing: GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (BuildContext context) =>
+                                    UserProfile()));
+                      },
+                      child: Icon(Icons.edit),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.lock,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Privacy',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: .5,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.security,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Terms of Use',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: .5,
+                  ),
+                  GestureDetector(
+                    onTap: () {},
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.info,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'About',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    thickness: .5,
+                    color: Colors.black,
+                  ),
+                  GestureDetector(
+                    onTap: _launchURL,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.attach_money,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Support Development',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Divider(
+                    color: Colors.black,
+                    thickness: .5,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      FirebaseAuth.instance.signOut();
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => Login()));
+                    },
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.exit_to_app,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Logout',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      ),
+                    ),
+                  )
                 ],
               ),
             )
@@ -103,5 +198,15 @@ class _AccountState extends State<Account> {
         ),
       ),
     );
+  }
+}
+
+_launchURL() async {
+  const url =
+      'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=2BGZ6UUJRHPN4&currency_code=USD&source=url';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
